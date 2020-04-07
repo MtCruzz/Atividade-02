d44d655a647dc33cbe6a070baba0fc06b4ab128b
MtCruzz <matheuscruz8@gmail.com>
2020-04-06 21:40:53 -0300

    Auto incremento

--- src/main/java/com/example/atividade02/Entidade/Funcionario.java
+++ src/main/java/com/example/atividade02/Entidade/Funcionario.java
@@ -1,6 +1,7 @@
 package com.example.atividade02.Entidade;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 /**
@@ -9,7 +10,7 @@ import javax.persistence.Id;
 @Entity
 public class Funcionario {
 
-    @Id
+    @Id @GeneratedValue
     private int id;
     private String nome;
     private String rg;
